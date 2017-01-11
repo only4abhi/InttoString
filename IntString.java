ddfea477a073406214ea022c0319328f930b7705
only4abhi <abhishekchanda.mailbox@gmail.com>
2017-01-11 21:30:15 +0530

    Add files via upload

---
+++ IntString.java
@@ -0,0 +1,15 @@
+
+public class IntString {
+
+	public static void main(String[] args) {
+		int i = Integer.parseInt("123");
+		 System.out.println("Integer: " + i);
+
+		String val = Integer.toString(i);  // Convert into String
+	    System.out.println("Stirng: " + (val+20)); // Concate string with 20  
+		 
+
+
+	}
+
+}
